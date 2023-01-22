b4a70dc05eb9359d794d515230a9db78790ab85d
Afonso Figuinha <963669+afonsofigs@users.noreply.github.com>
2023-01-22 00:37:29 +0000

    Message change all super

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from applescript import tell
 
 if __name__ == "__main__":
     next_check_wait_sec = 10
-    yourCommand = ("echo all great at: " + str(datetime.now()))
+    yourCommand = ("echo all super at: " + str(datetime.now()))
     while True:
         print("All very good at: ", datetime.now())
         tell.app('Terminal', 'do script "' + yourCommand + '"')
