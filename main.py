2e641d9740391ee95e4520898bf4fd417975599d
Afonso Figuinha <963669+afonsofigs@users.noreply.github.com>
2023-01-22 00:13:31 +0000

    Run terminals each 10 secs as the main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import time
 from datetime import datetime
 
+from applescript import tell
+
 if __name__ == "__main__":
-    next_check_wait_sec = 60
+    next_check_wait_sec = 10
+    yourCommand = ("echo All okaay at: " + str(datetime.now()))
+
     while True:
         print("All okaay at: ", datetime.now())
+        tell.app('Terminal', 'do script "' + yourCommand + '"')
         time.sleep(next_check_wait_sec)
