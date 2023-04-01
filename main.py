8d1a3c21a28f011e280bbe3acfb7fb701a8e60bf
Afonso Figuinha <963669+afonsofigs@users.noreply.github.com>
2023-04-01 20:17:18 +0100

    main.py calls the updates and reboots itself

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+import os
+import sys
 import time
 from datetime import datetime
 
 from applescript import tell
 
+from updater import update
+
 if __name__ == "__main__":
     next_check_wait_sec = 10
-    yourCommand = ("echo all super at: " + str(datetime.now()))
     while True:
-        print("All very good at: ", datetime.now())
+        message = "all great at: " + str(datetime.now())
+        yourCommand = ("echo " + message)
+        print(message)
+        # Open macOS terminal window and print the message
         tell.app('Terminal', 'do script "' + yourCommand + '"')
+
+        # Check for updates and perform them
+        if update():
+            # Start the updated version of main.py, self re-launching
+            # If using systemctl to control this main.py, simply set it to reboot always, no need for self re-launching
+            print("Starting new main.py")
+            os.system('python3 main.py')
+
+            print("Terminating old main.py")
+            sys.exit(0)
+
+        print("Check complete")
+
+        print("Waiting " + str(next_check_wait_sec) + " seconds until next check...")
         time.sleep(next_check_wait_sec)
--- updater.py
+++ updater.py
@@ -1,7 +1,3 @@
-import os
-import subprocess
-import sys
-import time
 from datetime import datetime as date
 
 from sh import git
@@ -13,10 +9,9 @@ def check_for_update(git_working_dir):
     git("--git-dir=" + git_working_dir + ".git/", "--work-tree=" + git_working_dir, "fetch", "origin", "main")
     print("Fetch complete.")
     print("Checking status for " + git_working_dir + "...")
-    status_check = git("--git-dir=" + git_working_dir + ".git/", "--work-tree=" + git_working_dir, "status")
-    # print(status_check)
-    if "Your branch is up to date" in status_check and "Changes not staged for commit" not in status_check:
-        print("Status check passes.")
+    git_status = git("--git-dir=" + git_working_dir + ".git/", "--work-tree=" + git_working_dir, "status")
+    # print(git_status)
+    if "Your branch is up to date" in git_status and "Changes not staged for commit" not in git_status:
         print("Code up to date.")
         return False
     else:
@@ -24,32 +19,21 @@ def check_for_update(git_working_dir):
         return True
 
 
+def update():
+    git_dir = "./"
+    print("***** Checking for code update at: " + str(date.now()) + " *****")
+    if check_for_update(git_dir):
+        print("Resetting code...")
+        # overwrite local code with remote code, losing local changes.
+        git("--git-dir=" + git_dir + ".git/", "--work-tree=" + git_dir, "reset", "--hard", "origin/main")
+        # fully clean untracked files and artifacts, not good idea if there are data files
+        # git("--git-dir=" + git_dir + ".git/", "--work-tree=" + git_dir, "clean", "-df")
+        print("Reset finished.")
+        # True means code has updated
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
-    next_check_wait_sec = 30
-    git1_dir = "./"
-    git1_url = "https://github.com/afonsofigs/updater_testes.git"
-
-    # Start current main
-    mp = subprocess.Popen(['python', 'main.py'])
-    # Start update procedure
-    while True:
-        print("***** Checking for code update at: " + str(date.now()) + " *****")
-        if check_for_update(git1_dir):
-            print("Stopping old main.py")
-            mp.terminate()
-
-            print("Resetting code...")
-            # overwrite local code with remote code, losing local changes.
-            git("--git-dir=" + git1_dir + ".git/", "--work-tree=" + git1_dir, "reset", "--hard",
-                "origin/main")
-            # fully clean untracked files and artifacts, not good idea if there are data files
-            # git("--git-dir=" + git1_dir + ".git/", "--work-tree=" + git1_dir, "clean", "-df")
-
-            # Start updater_v2
-            print("Starting new updater.py")
-            os.system('python updater.py')
-            sys.exit()
-        else:
-            print("No updates found.")
-        print("Check complete. Waiting " + str(next_check_wait_sec) + " seconds until next check...")
-        time.sleep(next_check_wait_sec)
+    check_for_update("./")
